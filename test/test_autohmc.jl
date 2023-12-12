6d00a74b4479ffb675a820dd11675929f3f327dc
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2023-12-11 18:32:33 -0800

    minor change to sim settings

--- test/test_autohmc.jl
+++ test/test_autohmc.jl
@@ -4,10 +4,10 @@ pt = pigeons(
     target = toy_mvn_target(2), 
     # target = Pigeons.stan_funnel(9), 
     n_chains = 1, 
-    explorer = AutoHMC(n_leapfrog = 5, base_n_refresh = 1, exponent_n_refresh = 0.0), 
-    # explorer = AutoMALA(base_n_refresh = 1, exponent_n_refresh = 0.0), 
+    # explorer = AutoHMC(n_leapfrog = 5, base_n_refresh = 1, exponent_n_refresh = 0.0), 
+    explorer = AutoMALA(base_n_refresh = 1, exponent_n_refresh = 0.0), 
     record = [traces, online], 
-    n_rounds = 12
+    n_rounds = 10
 )
 
 println(sample_names(pt))
