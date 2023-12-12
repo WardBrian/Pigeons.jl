63a4f3f01fb701304ba210412b28ab40e9aa64c3
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2023-12-11 18:30:09 -0800

    add plots check

--- test/test_autohmc.jl
+++ test/test_autohmc.jl
@@ -1,5 +1,8 @@
+using Plots 
+
 pt = pigeons(
-    target = toy_mvn_target(10), 
+    target = toy_mvn_target(2), 
+    # target = Pigeons.stan_funnel(9), 
     n_chains = 1, 
     explorer = AutoHMC(n_leapfrog = 5, base_n_refresh = 1, exponent_n_refresh = 0.0), 
     # explorer = AutoMALA(base_n_refresh = 1, exponent_n_refresh = 0.0), 
@@ -10,3 +13,10 @@ pt = pigeons(
 println(sample_names(pt))
 println(mean(pt)) 
 println(var(pt))
+samples = get_sample(pt) 
+
+p = Plots.plot(
+    [samples[i][1] for i in eachindex(samples)], 
+    [samples[i][2] for i in eachindex(samples)]
+)
+display(p)
